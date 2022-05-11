aa3fe603119cd84eaf89918cebb93e8ca5e1bf7a
James Bracy <waratuman@gmail.com>
2022-05-11 13:46:34 -0400

    Expectations for destroy tests

--- test/active_record/destroy_test.rb
+++ test/active_record/destroy_test.rb
@@ -21,71 +21,108 @@ class ActiveRecord::DestroyTest < ActiveSupport::TestCase
   end
 
   test 'Base::with_metadata nil' do
-    assert_not_query(/INSERT INTO "changebase_metadata"/i) do
-      ActiveRecord::Base.with_metadata(nil) do
-        @post.destroy
-      end
+    ActiveRecord::Base.with_metadata(nil) do
+      @post.destroy
+    end
+
+    case Changebase.mode
+    when 'replication'
+      assert_not_query(/INSERT INTO "changebase_metadata"/i)
+    when 'inline'
+      assert_posted('', {
+        transaction: {
+          lsn: 1,
+          timestamp: "2022-05-10T12:04:53.397-04:00",
+          events: [
+            { lsn: 6,
+              type: "delete",
+              schema: "public",
+              table: "posts",
+              timestamp: "2022-05-10T12:32:14.966-04:00",
+              columns: [
+                { index: 1,
+                  type: "int4",
+                  name: "id",
+                  value: 1,
+                }, {
+                  index: 2,
+                  type: "text",
+                  name: "title",
+                  value: "one",
+                }
+              ]
+            }
+          ]
+        }})
     end
   end
 
   test 'Base::with_metadata {}' do
-    assert_not_query(/INSERT INTO "changebase_metadata"/i) do
-      ActiveRecord::Base.with_metadata({}) do
-        @post.destroy
-      end
+    ActiveRecord::Base.with_metadata({}) do
+      @post.destroy
+    end
+
+    case Changebase.mode
+    when 'replication'
+      assert_not_query(/INSERT INTO "changebase_metadata"/i)
+    when 'inline'
+      assert_posted('', {
+        transaction: {
+          lsn: 1,
+          timestamp: "2022-05-10T12:04:53.397-04:00",
+          metadata: {},
+          events: [
+            { lsn: 6,
+              type: "delete",
+              schema: "public",
+              table: "posts",
+              timestamp: "2022-05-10T12:32:14.966-04:00",
+              columns: [
+                { index: 1,
+                  type: "int4",
+                  name: "id",
+                  value: 1,
+                }, {
+                  index: 2,
+                  type: "text",
+                  name: "title",
+                  value: "one",
+                }
+              ]
+            }
+          ]
+        }})
     end
   end
 
   test 'Base::with_metadata DATA' do
-    debug do
     ActiveRecord::Base.with_metadata({user: 'tom'}) do
       @post.destroy
     end
-  end
+
     case Changebase.mode
     when 'inline'
       assert_posted('', {
         transaction: {
           lsn: 1,
           timestamp: "2022-05-10T12:04:53.397-04:00",
-          metadata: {
-            "veniam" => "Corporis repellat ipsam est.",
-            "aperiam" => "Odio ut ut et."
-          },
+          metadata: { user: "tom" },
           events: [
             { lsn: 6,
               type: "delete",
-              schema: "est_rerum_suscipits",
-              table: "non_quibusdam_impedits",
+              schema: "public",
+              table: "posts",
               timestamp: "2022-05-10T12:32:14.966-04:00",
-              database_id: "85683d67-a51e-4e97-a016-d3666d286789",
               columns: [
                 { index: 1,
                   type: "int4",
-                  name: "possimus",
-                  value: -1608095202,
+                  name: "id",
+                  value: 1,
                 }, {
                   index: 2,
-                  type: "float4",
-                  name: "enim",
-                  value: 566881.1375015317,
-                }, { index: 3,
-                  type: "float4",
-                  name: "sequi",
-                  value: 216365.3829521479,
-                  database_id: "85683d67-a51e-4e97-a016-d3666d286789"
-                }, {
-                  index: 4,
                   type: "text",
-                  name: "iure",
-                  value: "Hic soluta sed eius.",
-                  database_id: "85683d67-a51e-4e97-a016-d3666d286789"
-                }, {
-                  index: 5,
-                  type: "int2",
-                  name: "occaecati",
-                  value: -2441,
-                  database_id: "85683d67-a51e-4e97-a016-d3666d286789"
+                  name: "title",
+                  value: "one",
                 }
               ]
             }
@@ -102,18 +139,77 @@ class ActiveRecord::DestroyTest < ActiveSupport::TestCase
   end
 
   test 'Model::with_metadata nil' do
-    assert_not_query(/INSERT INTO "changebase_metadata"/i) do
-      Post.with_metadata(nil) do
-        @post.destroy
-      end
+    Post.with_metadata(nil) do
+      @post.destroy
+    end
+
+    case Changebase.mode
+    when 'replication'
+      assert_not_query(/INSERT INTO "changebase_metadata"/i)
+    when 'inline'
+      assert_posted('', {
+        transaction: {
+          lsn: 1,
+          timestamp: "2022-05-10T12:04:53.397-04:00",
+          events: [
+            { lsn: 6,
+              type: "delete",
+              schema: "public",
+              table: "posts",
+              timestamp: "2022-05-10T12:32:14.966-04:00",
+              columns: [
+                { index: 1,
+                  type: "int4",
+                  name: "id",
+                  value: 1,
+                }, {
+                  index: 2,
+                  type: "text",
+                  name: "title",
+                  value: "one",
+                }
+              ]
+            }
+          ]
+        }})
     end
   end
 
   test 'Model::with_metadata {}' do
-    assert_not_query(/INSERT INTO "changebase_metadata"/i) do
-      Post.with_metadata({}) do
-        @post.destroy
-      end
+    Post.with_metadata({}) do
+      @post.destroy
+    end
+
+    case Changebase.mode
+    when 'replication'
+      assert_not_query(/INSERT INTO "changebase_metadata"/i)
+    when 'inline'
+      assert_posted('', {
+        transaction: {
+          lsn: 1,
+          timestamp: "2022-05-10T12:04:53.397-04:00",
+          metadata: {},
+          events: [
+            { lsn: 6,
+              type: "delete",
+              schema: "public",
+              table: "posts",
+              timestamp: "2022-05-10T12:32:14.966-04:00",
+              columns: [
+                { index: 1,
+                  type: "int4",
+                  name: "id",
+                  value: 1,
+                }, {
+                  index: 2,
+                  type: "text",
+                  name: "title",
+                  value: "one",
+                }
+              ]
+            }
+          ]
+        }})
     end
   end
 
@@ -122,14 +218,42 @@ class ActiveRecord::DestroyTest < ActiveSupport::TestCase
       @post.destroy
     end
 
-    assert_posted('', {}, mode: :inline)
-
-    assert_query(<<~MSG, mode: :replication)
-      INSERT INTO "changebase_metadata" ( version, data )
-      VALUES ( 1, '{"user":"tom"}' )
-      ON CONFLICT ( version )
-      DO UPDATE SET version = 1, data = '{"user":"tom"}';
-    MSG
+    case Changebase.mode
+    when 'replication'
+      assert_query(<<~MSG, mode: :replication)
+        INSERT INTO "changebase_metadata" ( version, data )
+        VALUES ( 1, '{"user":"tom"}' )
+        ON CONFLICT ( version )
+        DO UPDATE SET version = 1, data = '{"user":"tom"}';
+      MSG
+    when 'inline'
+      assert_posted('', {
+        transaction: {
+          lsn: 1,
+          timestamp: "2022-05-10T12:04:53.397-04:00",
+          metadata: { user: "tom" },
+          events: [
+            { lsn: 6,
+              type: "delete",
+              schema: "public",
+              table: "posts",
+              timestamp: "2022-05-10T12:32:14.966-04:00",
+              columns: [
+                { index: 1,
+                  type: "int4",
+                  name: "id",
+                  value: 1,
+                }, {
+                  index: 2,
+                  type: "text",
+                  name: "title",
+                  value: "one",
+                }
+              ]
+            }
+          ]
+        }})
+    end
   end
 
 end
