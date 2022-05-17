b2f8c7ccf510579301a4e1a0e33c78dfb799175d
James Bracy <waratuman@gmail.com>
2022-05-16 21:41:56 -0400

    More HABTM tests

--- test/active_record/associations/has_and_belongs_to_many_test.rb
+++ test/active_record/associations/has_and_belongs_to_many_test.rb
@@ -494,10 +494,95 @@ class HasAndBelongsToManyTest < ActiveSupport::TestCase
     })
   end
 
-  test 'has_and_belongs_to_many <<'
+  test 'has_and_belongs_to_many <<' do
+    timestamp = Time.current + 1.day
+    topic = Topic.create(name: "Known Unkowns")
+    post = Post.create(title: "Black Holes", topics: [])
+
+    travel_to(timestamp) do
+      post.topics << topic
+    end
+
+    assert_posted('/transactions', {
+      transaction: {
+        lsn: timestamp.utc.iso8601(3),
+        timestamp: timestamp.utc.iso8601(3),
+        events: [
+          {
+            lsn: timestamp.utc.iso8601(3),
+            type: "insert",
+            schema: "public",
+            table: "posts_topics",
+            timestamp: timestamp.utc.iso8601(3),
+            columns: [
+              {
+                index: 0,
+                type: "bigint",
+                name: "post_id",
+                value: post.id,
+                previous_value: nil,
+                identity: true
+              }, {
+                index: 1,
+                type: "bigint",
+                name: "topic_id",
+                value: topic.id,
+                previous_value: nil,
+                identity: true
+              }
+            ]
+          }
+        ]
+      }
+    })
+  end
+
   test 'has_and_belongs_to_many.delete'
   test 'has_and_belongs_to_many.destroy'
-  test 'has_and_belongs_to_many='
+
+  test 'has_and_belongs_to_many=' do
+    timestamp = Time.current + 1.day
+    topic = Topic.create(name: "Known Unknowns")
+    post = Post.create(title: "Black Holes")
+    reset!
+
+    travel_to(timestamp) do
+      post.topics = [ topic ]
+    end
+
+    assert_posted("/transactions", {
+      transaction: {
+        lsn: timestamp.utc.iso8601(3),
+        timestamp: timestamp.utc.iso8601(3),
+        events: [
+          {
+            lsn: timestamp.utc.iso8601(3),
+            type: "insert",
+            schema: "public",
+            table: "posts_topics",
+            timestamp: timestamp.utc.iso8601(3),
+            columns: [
+              {
+                index: 0,
+                identity: true,
+                name: "post_id",
+                type: "bigint",
+                value: post.id,
+                previous_value: nil
+              }, {
+                index: 1,
+                identity: true,
+                name: "topic_id",
+                type: "bigint",
+                value: topic.id,
+                previous_value: nil
+              }
+            ]
+          }
+        ]
+      }
+    })
+  end
 
   test 'has_and_belongs_to_many_ids=' do
     timestamp = Time.current + 1.day
